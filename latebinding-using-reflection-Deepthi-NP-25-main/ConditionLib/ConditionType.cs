fc85d33edbca0ffc0e6f2a2257f218bea47fbf61
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-07-14 14:18:42 +0530

    Add files via upload

---
+++ latebinding-using-reflection-Deepthi-NP-25-main/ConditionLib/ConditionType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConditionLib
+{
+    public class ConditionType
+    {
+        public static System.Type FindClass(System.Reflection.Assembly _assemblyRef, string name)
+        {
+            System.Type classType = _assemblyRef.GetType(name);
+            if (classType != null)
+            {
+                if (classType.IsClass)
+                {
+                    return classType;
+
+                }
+            }
+            return null;
+        }
+
+
+        public static object InvokeMethod(object source, System.Reflection.MethodInfo methodRef, object[] arguments)
+        {
+            return methodRef.Invoke(source, arguments);
+        }
+    }
+}
+
+
